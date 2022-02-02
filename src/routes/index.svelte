7990dd7445460690fa0eedc492851d3dd3aabca6
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-02-02 21:34:20 +0000

    Add width and height to image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,8 @@
 </svelte:head>
 
 <div>
-    <img src="/image-qr-code.png" alt="qr code">
+    <img src="/image-qr-code.png" alt="qr code" width="288" height="288">
+    
     <h1>Improve your front-end skills by building projects</h1>
 
     <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
