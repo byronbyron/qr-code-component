45bd09c7befeae9ea6894b1f71ec87f001bfda41
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-02-02 21:10:28 +0000

    Add layout component and normalize.css

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import 'normalize.css';
+    import '../global.css';
+</script>
+
+<main>
+    <!-- page content -->
+    <slot></slot>
+</main>
+
+<footer>
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://twitter.com/bwalkermills" target="_blank" rel="noopener noreferrer">Byron</a>.
+</footer>
+
+<style>
+    main {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    footer {
+        display: none;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+</script>
+
+<svelte:head>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <title>Frontend Mentor | QR code component</title>
+</svelte:head>
+
+<div class="card">
+    <img src="/image-qr-code.png" alt="qr code" class="card-image">
+    <h1 class="card-title">Improve your front-end skills by building projects</h1>
+
+    <p class="card-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+</div>
+
+<style>
+    .card {
+        
+    }
+
+    .card-image {
+
+    }
+
+    .card-title {
+
+    }
+
+    .card-text {
+
+    }
+
+</style>
